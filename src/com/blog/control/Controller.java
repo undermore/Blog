deab3d1a4320d5ee2e7d041aca80873222bf509d
pplower <pplower@126.com>
2020-12-13 20:36:22 +0800

    设置 response的编码方式 解决中文乱码问题

--- src/com/blog/control/Controller.java
+++ src/com/blog/control/Controller.java
@@ -18,6 +18,8 @@ public class Controller extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
+		response.setCharacterEncoding("utf-8");    //设置Response使用 utf-8 编码
+		response.setHeader("Content-Type", "text/html;charset=utf-8");  //告诉浏览器 Response 使用 utf-8编码
 		response.getWriter().append("我们在这里处理浏览器的请求，默认情况中文是乱码");
 	}
 
