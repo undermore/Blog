4e7da4769378c01a14410d70ae40c5ef90eb0862
pplower <pplower@126.com>
2020-12-14 00:03:29 +0800

    在  LoginServlet 中增加登录相关的逻辑

--- src/com/blog/control/LoginServlet.java
+++ src/com/blog/control/LoginServlet.java
@@ -19,6 +19,25 @@ public class LoginServlet extends HttpServlet {
 	
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	userDao = new UserDao();
+    	//取得表单提交的各参数
+    	String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String result = request.getParameter("VerifyCode");
+        
+        //取得由 ImgServlet 设置的验证码答案
+        String answer =(String)request.getSession().getAttribute("VerifyCode");
+        
+        //判断用户名和密码是否正确 以及 验证码答案是否正确
+        if((userDao.VerifyUser(username,password))&&(result.equals(answer))) 
+        {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            session.setMaxInactiveInterval(3*60);//单位s
+            response.sendRedirect(request.getContextPath()+"/servlet/Controller?act=10"); //在 Controller 中跳转到 管理员界面
+        }
+        else
+        	//如果错误 则跳转到 error 页面
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
